89e6460c54a448f6701b2bf7ea5c290d0eddb424
Dataniel31 <alexdspe02@gmail.com>
2024-09-29 20:29:07 -0500

    update

--- pingpong.py
+++ pingpong.py
@@ -20,6 +20,15 @@ COLOR_FONDO = (150, 200, 170)
 AZUL = (70, 130, 180)
 ROJO = (200, 70, 90)
 
+# Definir sonidos
+pg.mixer.init()
+sonido_golpe_paleta = pg.mixer.Sound("golpe_paleta.mp3")
+sonido_golpe_pared = pg.mixer.Sound("golpe_pared.mp3")
+sonido_punto = pg.mixer.Sound("punto.mp3")
+sonido_golpe_paleta.set_volume(0.5)
+sonido_golpe_pared.set_volume(0.5)
+sonido_punto.set_volume(0.5)
+
 # Coordenadas | Tamaño de jugadores
 j1_x = 50
 j1_y = 250
@@ -36,6 +45,14 @@ ALTO_PELOTA = 12
 pelota_diferencia_x = 4
 pelota_diferencia_y = 4
 
+# puntajes iniciales
+puntos_j1 = 0
+puntos_j2 = 0
+
+# definir las fuentes
+pg.font.init()
+calibri_bold_35 = pg.font.SysFont('Calibri Bold', 35)
+
 # Crear los elementos
 paleta_j1 = pg.Rect(j1_x, j1_y, ANCHO_PALETA, ALTO_PALETA)
 paleta_j2 = pg.Rect(j2_x, j2_y, ANCHO_PALETA, ALTO_PALETA)
@@ -47,6 +64,10 @@ def dibujar_pantalla():
     pg.draw.rect(pantalla, AZUL, paleta_j1)
     pg.draw.rect(pantalla, ROJO, paleta_j2)
     pg.draw.rect(pantalla, BLANCO, pelota)
+    texto_puntos_j1 = calibri_bold_35.render("PUNTOS J1: " + str(puntos_j1), True, AZUL)
+    texto_puntos_j2 = calibri_bold_35.render("PUNTOS J2: " + str(puntos_j2), True, ROJO)
+    pantalla.blit(texto_puntos_j1, (130, 20))
+    pantalla.blit(texto_puntos_j2, (620, 20))
 
 
 def resetear_pelota_y_paletas():
@@ -91,14 +112,23 @@ while ejecutando:
 
     # verificar colisiones
     if pelota.colliderect(paleta_j1):
+        sonido_golpe_paleta.play()
         pelota_diferencia_x = abs(pelota_diferencia_x)
     elif pelota.colliderect(paleta_j2):
+        sonido_golpe_paleta.play()
         pelota_diferencia_x = abs(pelota_diferencia_x) * -1
     elif pelota_y <= 0:
+        sonido_golpe_pared.play()
         pelota_diferencia_y = abs(pelota_diferencia_y)
     elif pelota_y >= ALTO_PANTALLA:
+        sonido_golpe_pared.play()
         pelota_diferencia_y = abs(pelota_diferencia_y) * -1
     elif pelota_x <= 0 or pelota_x >= ANCHO_PANTALLA:
+        sonido_punto.play()
+        if pelota_x >= ANCHO_PANTALLA:
+            puntos_j1 += 1
+        elif pelota_x <= 0:
+            puntos_j2 += 1
         resetear_pelota_y_paletas()
 
     dibujar_pantalla()
